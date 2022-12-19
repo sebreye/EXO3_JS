c90b1c4bfc70f8c4018a8044809b1b94c55cc04b
sebastian Reyes <sebreyes222gmail.com>
2022-12-19 13:58:05 +0100

    version final

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+let chiffre1 = prompt(5)
+let chiffre2 = prompt(6)
+chiffre1+chiffre2;
+chiffre1*chiffre2;
+chiffre1/chiffre2;
+chiffre1-chiffre2;
+alert(chiffre1-chiffre2)
+alert(chiffre1+chiffre2)
+alert(chiffre1/chiffre2)
+alert(chiffre1*chiffre2)
+
